32804000efb72e3b2760cadd581fce2ee1c9499c
Atul Roshan Naik <atulroshannaik.ai24@rvce.edu.in>
2025-11-01 22:54:39 +0530

    fix gas error

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -172,7 +172,7 @@ export default function Home() {
         metadataCid,
         chatLink || "",
         {
-          gasLimit: 3000000, // Explicitly set a high gas limit for file uploads
+          gas: 3000000, // Explicitly set a high gas limit for file uploads
         }
       );
       
